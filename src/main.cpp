9be585d35c5af187aaf03738b666093b34494402
* <mateusgms12@gmail.com>
2022-04-13 21:38:17 -0300

    main.cpp updated
    Created basic functions to initialize the struct values.
    Created basic function prototypes to handle data.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,43 @@
 #include <iostream>
+#include <string.h>
+
+struct Data{
+	float temperature;
+	float umidity;
+	float light;
+}
+
+union Packet{
+	Data data;
+	char buffer[sizeof(Data)];
+}
+
+union Packet packet_rx;
+union Packet packet_tx;
+
+void Send();
+void Receive();
+void InitPkt();
 
 int main() {
+	Init();
 	return 0;
+}
+
+void InitPkt(){
+	std::cout << "TX Packet being initialized..." << std::endl;
+	packet_tx.data.temperature = 10.2;
+	packet_tx.data.umidity = 23.7;
+	packet_tx.data.light = 56.3;
+	std::cout << "TX Packet ready!" << std::endl;
+}
+
+void Send(){
+	std::cout << "Sending TX ..." << std::endl;
+	//to be implemented
+}
+
+void Receive(){
+	std::cout << "Reading RX ..." << std::endl;
+	//to be implemented
 }
